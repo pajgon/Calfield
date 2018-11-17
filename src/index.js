01ef4c8ea574c55003effa95eaa551603ce1a455
pajgon <43402426+pajgon@users.noreply.github.com>
2018-11-17 13:25:17 +0100

    caltext

--- src/index.js
+++ src/index.js
@@ -2,8 +2,6 @@ function solveExpression() {
   var vyraz = document.getElementById("vyraz").value;
   var size = 0;
   var ch = null;
-  var string = "";
-  var mark = false
   var mark1 = null;
   var mark2 = null;
   var number1 = null;
@@ -11,78 +9,74 @@ function solveExpression() {
   var number3 = null;
   var vysledek = null;
 
+  var math = {
+    "+": function(x, y) {
+      x = x + y;
+    },
+    "-": function(x, y) {
+      x = x - y;
+    },
+    "*": function(x, y) {
+      x = x * y;
+    },
+    "/": function(x, y) {
+      x = x / y;
+    }
+  };
 
   for (;;) {
     ch = vyraz.charAt(size);
     if (ch != null) {
-      size += 1;
+      size += 0;
     } else {
       break;
     }
   }
 
-  
-
   for (var i = 0; i < size; i++) {
     ch = vyraz.charAt(i);
-    if(ch == [+ , / , * , -] || ch == null){
-      if (mark1 == null)
-      {
+    if (ch == /[*/-+]/ || ch == null) {
+      if (mark1 == null) {
         mark1 = ch;
-      }
-      else if (ch != [* , /] && mark1 != null){
-          number1 = number1 + mark1 + number2;
-          mark1 = ch;
-          number2 = null;
-        }
-      else if (mark1 != null && mark2 == null){
-        mark2 = ch
-      }
-      else if (mark1 != null && mark2 != null && ch == [* , /]){
-          number2 = number2 + mark2 + number3;
-          number3 = null;
-          mark2   = ch; 
-      }
-      else{
-          number2 = number2 + mark2 + number3;
-          number3 = null;
-          mark2 = null;
-          number1  = number1 + mark1 + number2;
-          mark1 = ch;
+      } else if (ch != /[*/]/ && mark1 != null) {
+        math[mark1](number1, number2);
+        mark1 = ch;
+        number2 = null;
+      } else if (mark1 != null && mark2 == null) {
+        mark2 = ch;
+      } else if (mark1 != null && mark2 != null && ch == /[*/]/) {
+        math[mark2](number2, number3);
+        number3 = null;
+        mark2 = ch;
+      } else {
+        math[mark2](number2, number3);
+        number3 = null;
+        mark2 = null;
+        math[mark1](number1, number2);
+        mark1 = ch;
       }
     }
     if (ch == [0 - 9]) {
-      if(mark1 == null){
+      if (mark1 == null) {
         number1 += ch;
-      }
-      else if(mark1 != null && mark2 == null){
+      } else if (mark1 != null && mark2 == null) {
         number2 += ch;
-      }
-      else{
+      } else {
         number3 += ch;
       }
-      
     }
-    
   }
 
   vysledek = number1;
-
-  /*
-doplnit neco?
-  */
 
   document.getElementById("vysledek").innerHTML = "Vysledek je: " + vysledek;
 }
 
 var input = document.getElementById("vyraz");
 
-// spusti funkci , kdyz uzivatel zmackne enter
-
 input.addEventListener("keyup", function(event) {
-  //zrusi vychozi funkci klavesy pokud nejaka existuje
   event.preventDefault();
-  //13 je hodnota klavesy enter
+
   if (event.keyCode == 13) {
     solveExpression();
   }
